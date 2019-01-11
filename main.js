53144e3e2339e1aedd3548690ad5bb127c5c015f
vilaca <jvilaca@gmail.com>
2019-01-11 22:56:53 +0000

    Constants for width and height. Removed stretch.

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ const fx = document.getElementById("fx");
 const ctxCanvas = canvas.getContext("2d");
 const ctxFx = fx.getContext("2d");
 
-canvas.width = 300;
-canvas.height = 200;
+const width = 600;
+const height = 300;
 
-fx.width = 300*4;
-fx.height = 200*4;
+canvas.width = width;
+canvas.height = height;
+
+fx.width = width;
+fx.height = height;
 
 var bbb = 0;
+const th = 200;
 
 function takepicture() {
 
@@ -24,38 +28,24 @@ function takepicture() {
         gt = 0,
         bt = 0;
 
-    var th = 200;
-
-	var imgDataOld = ctxCanvas.getImageData(0,0,300,200);
+	const latest = ctxCanvas.getImageData(0,0,width,height);
+	const noob = ctxCanvas.getImageData(0,0,width,height);
 
     for (var x = 0; x < canvas.width; x++) {
         for (var y = 0; y < canvas.height; y++) {
 
-			var r = imgDataOld.data[((y * (imgDataOld.width * 4)) + (x * 4)) + 0];
-			var g = imgDataOld.data[((y * (imgDataOld.width * 4)) + (x * 4)) + 1];
-			var b = imgDataOld.data[((y * (imgDataOld.width * 4)) + (x * 4)) + 2];
+			var r = latest.data[((y * (latest.width * 4)) + (x * 4)) + 0];
+			var g = latest.data[((y * (latest.width * 4)) + (x * 4)) + 1];
+			var b = latest.data[((y * (latest.width * 4)) + (x * 4)) + 2];
 
-            ctxFx.fillStyle = "rgba(" +
-                (Math.abs(r - rt) < th ? 255 : 0) +
-                "," +
-                (Math.abs(g - gt) < th ? 255 : 0) +
-                "," +
-                (Math.abs(b - bt) < th ? 255 : 0) +
-                ",255)";
+            ctxFx.fillStyle = "rgba(" + (Math.abs(r - rt) < th ? 255 : 0) + "," + (Math.abs(g - gt) < th ? 255 : 0) + "," + (Math.abs(b - bt) < th ? 255 : 0) + ",255)";
 
             rt |= b;
             gt |= r;
             bt |= g;
+			
+			ctxFx.fillRect(x, y, 1, 1);
 
-			var d = (Math.abs(r - rt) + Math.abs(g - gt) + Math.abs(b - bt)) / 2.5;
-
-            if (bbb % 48 > 24) {
-				d = 0;
-            } else 
-				if (d>=x*4) {
-					d = 0; 
-            }
-            ctxFx.fillRect(x*4-d, y*4, 4+d, 4);
         }
     }
 
